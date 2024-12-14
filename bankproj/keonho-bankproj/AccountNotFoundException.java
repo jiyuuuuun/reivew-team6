a0250c5f842882b54d7602c8d96b223a484ada57
jerry6475 <jerry6475@naver.com>
2024-12-15 00:44:58 +0900

    Update keonho-bankproj: Add new features

--- bankproj/keonho-bankproj/AccountNotFoundException.java
+++ bankproj/keonho-bankproj/AccountNotFoundException.java
@@ -1,9 +1,7 @@
 package backendproject;
 
 public class AccountNotFoundException extends Exception {
-    public AccountNotFoundException() {
-        super("Account not found");
-    }
+
     public AccountNotFoundException(String message) {
         super(message);
     }
--- bankproj/keonho-bankproj/NoCostomerException.java
+++
@@ -1,7 +0,0 @@
-package backendproject;
-
-public class NoCostomerException extends Exception{
-    public NoCostomerException(String message){
-        super(message);
-    }
-}
